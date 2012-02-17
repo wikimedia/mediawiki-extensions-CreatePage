728f4d17105f3bed4965eed0dbde9fe1177d575b
Raimond Spekking <raymond@users.mediawiki.org>
2012-02-17 21:26:28 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- CreatePage.i18n.php
+++ CreatePage.i18n.php
@@ -21,3 +21,28 @@ $messages['en'] = array(
 	'cp-desc' => 'Allows placing an input that takes the name of a new page into pages',
 	'cp-create' => 'Create page',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ * @author Metalhead64
+ */
+$messages['de'] = array(
+	'cp-desc' => 'Ermöglicht ein Eingabefeld für den Seitennamen zum Anlegen einer neuen Seite',
+	'cp-create' => 'Seite erstellen',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'cp-desc' => 'Permitte inserer in paginas un campo de entrata que demanda le nomine de un nove pagina a crear',
+	'cp-create' => 'Crear pagina',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'cp-create' => 'Pagina aanmaken',
+);
+
