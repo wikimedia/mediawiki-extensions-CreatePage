d118a3a9777dc4ef38ba2cbe1a2619fcbe85fc16
Ike Hecht <tosfos@yahoo.com>
2015-02-23 10:30:28 -0800

    Add autocompletion, options to modify existing pages & use VisualEditor
    * Autocomplete on page names in appropriate namespaces.
    * When user selects a page that exists, optionally open it in edit mode.
    * Optionally open pages in VisualEditor.
    
    Change-Id: I969e4904bdb0613fd09f997aa2326f2e2dfe1d71

--- CreatePage.php
+++ CreatePage.php
@@ -25,13 +25,14 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 global $wgVersion, $wgExtensionCredits, $wgExtensionMessagesFiles, $wgAutoloadClasses;
-global $wgSpecialPages, $wgHooks;
+global $wgSpecialPages, $wgHooks, $wgResourceModules;
 
-if ( version_compare( $wgVersion, '1.18c', '<' ) ) { // Needs to be 1.18c because version_compare() works in confusing ways.
+// Needs to be 1.18c because version_compare() works in confusing ways.
+if ( version_compare( $wgVersion, '1.18c', '<' ) ) {
 	die( '<b>Error:</b> Create Page requires MediaWiki 1.18 or above.' );
 }
 
-define( 'CP_VERSION', '0.3.0' );
+define( 'CP_VERSION', '0.4.0' );
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
@@ -39,19 +40,32 @@ $wgExtensionCredits['other'][] = array(
 	'version' => CP_VERSION,
 	'author' => array(
 		'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]',
+		'Ike Hecht'
 	),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Create_Page',
 	'descriptionmsg' => 'cp-desc'
 );
+//Configuration
+/* Set to true to edit existing pages. */
+$wgCreatePageEditExisting = false;
+/* Set to true to redirect to VisualEditor for page creation. */
+$wgCreatePageUseVisualEditor = false;
 
 // i18n
 $wgMessagesDirs['CreatePage'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['CreatePage'] = dirname( __FILE__ ) . '/CreatePage.i18n.php';
-$wgExtensionMessagesFiles['CreatePageAlias'] = dirname( __FILE__ ) . '/CreatePage.alias.php';
-$wgExtensionMessagesFiles['CreatePageMagic'] = dirname( __FILE__ ) . '/CreatePage.magic.php';
+$wgExtensionMessagesFiles['CreatePage'] = __DIR__ . '/CreatePage.i18n.php';
+$wgExtensionMessagesFiles['CreatePageAlias'] = __DIR__ . '/CreatePage.alias.php';
+$wgExtensionMessagesFiles['CreatePageMagic'] = __DIR__ . '/CreatePage.magic.php';
 
-$wgAutoloadClasses['SpecialCreatePageRedirect'] = dirname( __FILE__ ) . '/SpecialCreatePageRedirect.php';
+$wgAutoloadClasses['SpecialCreatePageRedirect'] = __DIR__ . '/SpecialCreatePageRedirect.php';
 $wgSpecialPages['CreatePageRedirect'] = 'SpecialCreatePageRedirect';
+
+$wgResourceModules['ext.createPage'] = array(
+	'scripts' => 'modules/createPage.searchSuggest.js',
+	'dependencies' => 'mediawiki.searchSuggest',
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'CreatePage'
+);
 
 $wgHooks['ParserFirstCallInit'][] = function( Parser &$parser ) {
 	$parser->setFunctionHook( 'createpage', function( Parser $parser, PPFrame $frame, array $args ) {
@@ -59,23 +73,33 @@ $wgHooks['ParserFirstCallInit'][] = function( Parser &$parser ) {
 			'action' => SpecialPage::getTitleFor( 'CreatePageRedirect' )->getLocalURL(),
 			'method' => 'post',
 			'style' => 'display: inline',
-		) );
+			'class' => 'createpageform'
+			) );
 
 		$html .= Html::input(
 			'pagename',
-			array_key_exists( 1, $args ) ? trim( $frame->expand( $args[1] ) ) : ''
+			array_key_exists( 1, $args ) ? trim( $frame->expand( $args[1] ) ) : '', 'text',
+				array( 'class' => 'pagenameinput' )
 		);
 
 		if ( array_key_exists( 0, $args ) ) {
-			$html .= Html::hidden( 'pagens', trim( $frame->expand( $args[0] ) ) );
+			$namespaceText =  trim( $frame->expand( $args[0] ) );
+			$attribs = array();
+
+			// Find the ID of this namespace, if there is one.
+			$namespaceID = MWNamespace::getCanonicalIndex( strtolower( $namespaceText ) );
+			if ( $namespaceID != 0 ) {
+				$attribs['nsid'] = $namespaceID;
+			}
+			$html .= Html::hidden( 'pagens', $namespaceText, $attribs );
 		}
 
 		$html .= '&#160;';
 
 		$html .= Html::input(
 			'createpage',
-			array_key_exists( 2, $args ) ? trim( $frame->expand( $args[2] ) ) : wfMessage( 'cp-create' )->text(),
-			'submit'
+			array_key_exists( 2, $args ) ? trim( $frame->expand( $args[2] ) ) :
+					wfMessage( 'cp-create' )->text(), 'submit'
 		);
 
 		if ( array_key_exists( 3, $args ) ) {
@@ -87,5 +111,10 @@ $wgHooks['ParserFirstCallInit'][] = function( Parser &$parser ) {
 		return $parser->insertStripItem( $html );
 	}, Parser::SFH_OBJECT_ARGS );
 
+	return true;
+};
+
+$wgHooks['BeforePageDisplay'][] = function( OutputPage &$out ) {
+	$out->addModules( 'ext.createPage' );
 	return true;
 };
--- SpecialCreatePageRedirect.php
+++ SpecialCreatePageRedirect.php
@@ -12,32 +12,47 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class SpecialCreatePageRedirect extends UnlistedSpecialPage {
-	
+
 	public function __construct() {
 		parent::__construct( 'CreatePageRedirect' );
 	}
-	
+
 	public function execute( $subPage ) {
 		$req = $this->getRequest();
-		
-		if ( $req->wasPosted() && $req->getCheck( 'pagename' ) ) {
-			$parts = array( $req->getText( 'pagename' ) );
-			
-			if ( $req->getCheck( 'pagens' ) ) {
-				array_unshift( $parts, $req->getText( 'pagens' ) );
+		if ( $req->getCheck( 'pagename' ) ) {
+			$pageName = $req->getText( 'pagename' );
+			$pageNamespace = $req->getText( 'pagens' );
+			if ( $pageNamespace == '' ||
+				substr( $pageName, 0, strlen( "$pageNamespace:" ) ) == "$pageNamespace:" ) {
+				$title = Title::newFromText( $pageName );
+			} else {
+				$title = Title::newFromText( "$pageNamespace:$pageName" );
 			}
-			
-			$target = Title::newFromText( implode( ':', $parts ) )->getLocalUrl( array(
-				'action' => 'edit',
-				'redlink' => '1',
-				'preload' => $req->getText( 'preload', '' )
-			) );
-		}
-		else {
+			$target = $this->getTargetURL( $title );
+		} else {
 			$target = Title::newMainPage()->getLocalURL();
 		}
-		
 		$this->getOutput()->redirect( $target, '301' );
 	}
-	
+
+	private function getTargetURL( Title $title ) {
+		global $wgCreatePageEditExisting, $wgCreatePageUseVisualEditor;
+
+		$isKnown = $title->isKnown();
+		$query = array();
+		if ( !$isKnown || $wgCreatePageEditExisting ) {
+			# Preload is not yet supported by VisualEditor, but probably will be eventually.
+			# See https://phabricator.wikimedia.org/T51622
+			$query['preload'] = $this->getRequest()->getText( 'preload', '' );
+			if ( !$isKnown ) {
+				$query['redlink'] = '1';
+			}
+			if ( $wgCreatePageUseVisualEditor ) {
+				$query['veaction'] = 'edit';
+			} else {
+				$query['action'] = 'edit';
+			}
+		}
+		return $title->getLocalUrl( $query );
+	}
 }
